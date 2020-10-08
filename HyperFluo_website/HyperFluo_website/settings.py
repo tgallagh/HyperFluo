b18b71763cf0d837c70ffec7c7072d4b805b9ad1
swleemans <simon.leemans@gmail.com>
2020-10-08 14:27:52 -0600

    secret key to environment variable 	modified:   HyperFluo_website/HyperFluo_website/__pycache__/settings.cpython-38.pyc 	modified:   HyperFluo_website/HyperFluo_website/settings.py

--- HyperFluo_website/HyperFluo_website/settings.py
+++ HyperFluo_website/HyperFluo_website/settings.py
@@ -21,7 +21,7 @@ PROJECT_DIR = os.path.join(BASE_DIR, "HyperFluo_website")
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "m7@uq&-xth9vp6-8ft^l3uykd39j!r7l8ad!tyx)zdo)*ludgf"
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
