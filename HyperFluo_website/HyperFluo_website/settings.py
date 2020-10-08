d0cd3639f74be489fb53dc6060745fd56a3508a5
swleemans <simon.leemans@gmail.com>
2020-10-08 14:58:12 -0600

    adding settings back in 	modified:   HyperFluo_website/HyperFluo_website/settings.py

--- HyperFluo_website/HyperFluo_website/settings.py
+++ HyperFluo_website/HyperFluo_website/settings.py
@@ -21,7 +21,9 @@ PROJECT_DIR = os.path.join(BASE_DIR, "HyperFluo_website")
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+with open('/Users/SimonL/localFluo/HyperFluo/HyperFluo_website/secret-key.txt 
+') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
